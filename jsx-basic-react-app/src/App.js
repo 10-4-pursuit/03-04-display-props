d3addcd3f691931bff41cc0b83fdac92251c93b9
AmiraFredericks <amirafredericks@pursuit.org>
2023-12-06 21:42:04 -0500

    Display Props

---
+++ jsx-basic-react-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import UserProfile from './UserProfile';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <UserProfile name = "Amira" age = {26} />
+      <UserProfile name = "Londyn" age = {4} />
+       
+
+    </div>
+  );
+}
+
+export default App;
