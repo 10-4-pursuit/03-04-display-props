e268e941675e56adbab59237a67b0c773df89435
Angl99 <anggelplasencia@pursuit.org>
2023-12-09 15:00:22 -0500

    finished exercise

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import UserProfile from './userProfile';
+
+function App() {
+  return (
+    <div className="App">
+      <UserProfile userName='Mike' age='27'/>
+      
+    </div>
+  );
+}
+
+export default App;
