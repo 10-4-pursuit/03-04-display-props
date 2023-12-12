19a0d20c9628eda1826225da18573a0e95239c6c
USERNAME <EMAIL>
2023-12-11 19:57:11 -0500

    all task are completed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import UserProfile from './UserProfile.js';
+function App() {
+  return (
+    <div className="App">
+   
+      <div>
+        <UserProfile name= 'Kanique'  age= '37'/>
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
