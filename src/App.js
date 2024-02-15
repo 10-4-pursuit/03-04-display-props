39b90377f0d0eb782b071f23d281a3f80b24555b
Smith-Janine <janinesmith@pursuit.org>
2024-02-14 19:37:02 -0500

    UserProfile component created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import UserProfile from './UserProfile';
+
+function App() {
+  return (
+    <div className="App">
+      <UserProfile username="Yah-neen" age="none ya business" />
+    </div>
+  );
+}
+
+export default App;
